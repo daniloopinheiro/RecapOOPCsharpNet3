28b714c60411dd099cfb55fa0be201503af64b75
Danilo O. Pinheiro <64677271+daniloopinheiro@users.noreply.github.com>
2023-06-25 17:41:27 -0300

    Update byte.cs

--- src/lib/BYte/byte.cs
+++ src/lib/BYte/byte.cs
@@ -4,14 +4,14 @@ namespace BYte
 {
     public class GetByte
     {
-        private byte nMaximo = byte.MaxValue;
-        private byte nMinimo = byte.MinValue;
+        private byte nMaximo;
+        private byte nMinimo;
         private bool assinado;
         private byte valor = 1;
         public byte ByteMax { get => nMaximo; set => nMaximo = value; }
         public byte ByteMin { get => nMinimo; set => nMinimo = value; }
         public string Titulo {
-            get => $"Assinatura: {assinado}, Bytes: {valor}, Valores Possiveis: {ByteMin} à {ByteMax}";
+            get => $"Assinatura: {assinado}, Bytes: {valor}, Valores Possiveis: {ByteMin = byte.MaxValue} à {ByteMax = byte.MinValue}";
             set => Titulo = value;
         }
     }
